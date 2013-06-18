c3c7e37e3362e7bfea18228ea1db90493e580258
Pete Cleary <pete.cleary@gmail.com>
2013-06-18 20:00:43 +0100

    update

--- www/js/particals.js
+++ www/js/particals.js
@@ -2,9 +2,51 @@
 var particleObj;
 
 document.addEventListener("deviceready", onDeviceReady, false);
-
+/*
 function onDeviceReady() {
     particleObj = new particleTest();
+}
+*/
+
+// Cordova is ready
+//
+function onDeviceReady() {
+    startWatch();
+}
+
+// Start watching the acceleration
+//
+function startWatch() {
+
+    // Update acceleration every 3 seconds
+    var options = { frequency: 100 };
+
+    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+
+// Stop watching the acceleration
+//
+function stopWatch() {
+    if (watchID) {
+        navigator.accelerometer.clearWatch(watchID);
+        watchID = null;
+    }
+}
+
+// onSuccess: Get a snapshot of the current acceleration
+//
+function onSuccess(acceleration) {
+    var element = document.getElementById('accelerometer');
+    element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                        'Acceleration Y: ' + acceleration.y + '<br />' +
+                        'Acceleration Z: ' + acceleration.z + '<br />' +
+                        'Timestamp: ' + acceleration.timestamp + '<br />';
+}
+
+// onError: Failed to get the acceleration
+//
+function onError() {
+    alert('onError!');
 }
 
 
